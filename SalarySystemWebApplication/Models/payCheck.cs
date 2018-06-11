c8b1b6348ebc60803956e78441c1ad85dd130a3b
Elsa Einasrdóttir <elsam@simnet.is>
2018-06-11 10:26:19 +0000

    design fix

---
+++ SalarySystemWebApplication/Models/payCheck.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SalarySystemWebApplication.Models
+{
+    public class payCheck
+    {
+        public int Id { get; set; }
+        public int IdLaunthegi { get; set; }
+        public byte[] launasedillprent { get; set; }
+        public DateTime timiFra { get; set; }
+        public DateTime timiTil { get; set; }
+        public int IdLaunagreidsla { get; set; }
+        public int IdRskSkilagrein { get; set; }
+        public int IdLifeyrirSkilagrein { get; set; }
+        public int astand { get; set; }
+    }
+}
